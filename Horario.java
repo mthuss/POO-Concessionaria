724163c0f3b541506ce5e826f2a932e3ed490178
Ana Catarina Marques Vicentim <catarinaanamarques830@gmail.com>
2022-07-04 21:02:47 -0300

    Update Horario.java
    Alteração da validação do Horario

--- Horario.java
+++ Horario.java
@@ -1,25 +1,61 @@
-public class Horario
-{
-    int hora,minutos;
+public class Horario {
+	private int horas;
+	private int minutos;
 
-    public Horario(int hora, int minutos) {
-        this.hora = hora;
-        this.minutos = minutos;
-    }
+    //Utilização do horario: retorna apenas true ou false e um aviso de horário inválido, caso false
+    //quando usamos a data, forçamos um do while que só seta o horário quando é retornado true
 
-    public int getHora() {
-        return hora;
-    }
+	// ================= Métodos da Classe
+	public String criarHorario() {
+		return String.format("%02d", this.horas) + ":" + String.format("%02d", this.minutos);
+	}
 
-    public void setHora(int hora) {
-        this.hora = hora;
-    }
+    public static boolean validarHora(int horas) {
+        if (horas >= 0 && horas <= 23) {
+          return true;
+        } else {
+          System.out.println("\n==!== AVISO: Insira uma hora válida!\n");
+          return false;
+        }
+      }
+    
+      public static boolean validarMinutos(int minutos) {
+        if (minutos >= 0 && minutos <= 59) {
+          return true;
+        } else {
+          System.out.println("\n==!== AVISO: Insira minutos válidos!\n");
+          return false;
+        }
+      }
 
-    public int getMinutos() {
-        return minutos;
-    }
+	// ================= Construtores
+	public Horario() {
+		this(0, 0);
+	}
 
-    public void setMinutos(int minutos) {
-        this.minutos = minutos;
-    }
-}
+	public Horario(int horas, int minutos) {
+		this.setHoras(horas);
+		this.setMinutos(minutos);
+	}
+
+	// ================= Getters e Setters
+	public int getHoras() {
+		return this.horas;
+	}
+
+	public int getMinutos() {
+		return this.minutos;
+	}
+
+	public void setHoras(int horas) {
+		if (!(Horario.validarHora(horas))){
+			this.horas = horas;	
+		}
+	}
+
+	public void setMinutos(int minutos) {
+		if (!(Horario.validarMinutos(minutos))) {
+			this.minutos = minutos;
+		} 
+	}
+}  
