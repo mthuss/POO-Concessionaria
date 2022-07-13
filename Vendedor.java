978713acac4344c67bd1df5e347135873f06be93
Douglas Silva <douglas.eduardo.2003@gmail.com>
2022-07-13 08:45:03 -0300

    updateVendedor

--- Vendedor.java
+++ Vendedor.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+
 public class Vendedor extends Funcionario
 {
     private float tempoRestante; //em horas(?)
     private Gerente gerente;
+    
+    ArrayList<Vendedor> listaVendedores = new ArrayList<>();
+    String dadosVendedores = "vendedores.txt";
+    File arq = new File(dadosVendedores);
 
+    //Construtor
     public Vendedor(long RG, String nome, Data dataNasc, Data dataAdmissao, float salario, float tempoRestante,
             Gerente gerente) {
         super(RG, nome, dataNasc, dataAdmissao, salario);
@@ -10,6 +19,11 @@ public class Vendedor extends Funcionario
         this.gerente = gerente;
     }
 
+    public Vendedor(){
+        this(0, "", null, null, 0, 0, null);
+    }
+
+    //Getters e Setters
     public float getTempoRestante() {
         return tempoRestante;
     }
