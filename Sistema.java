ff10307c01f8e86e2249b516af97c288594e173c
Douglas Silva <douglas.eduardo.2003@gmail.com>
2022-07-13 13:37:49 -0300

    coloquei menu adm para mexer no vendedor e metodos add, alterar, remover, listar

--- Sistema.java
+++ Sistema.java
@@ -10,12 +10,15 @@ public class Sistema {
     public static ArrayList <Carro> carros = new ArrayList<>();
     private int cilindradas;
     private String tipo;
-    int moto;
+    int moto, vendedor;
     Scanner input = new Scanner(System.in);
     ArrayList<Motocicleta> listaMotocicletas = new ArrayList<>();
+    ArrayList<Vendedor> listaVendedores = new ArrayList<>();
     //Parte de arquivos 
     String arquivoMotocicletas = "motocicletas.txt";
     File arq = new File(arquivoMotocicletas);
+    String dadosVendedores = "vendedores.txt";
+    File arqVendedor = new File(dadosVendedores);
 
 
     public static void menuVendedor()
@@ -625,10 +628,10 @@ public class Sistema {
                     cadastrarVendedor();
                 break;
                 case 2:
-                    //listarVendedor();
+                    listarVendedores();
                 break;
                 case 3:
-                    //alterarVendedor();
+                    alterarVendedor();
                 break;
                 case 4:
                     //removerVendedor();
@@ -642,28 +645,27 @@ public class Sistema {
     }
 
     public void cadastrarVendedor(){
-        Scanner scan = new Scanner(System.in);
         String senha,confirSenha;
         Vendedor novoVendedor = new Vendedor();
         System.out.print("Digite o nome do vendedor: ");
-        novoVendedor.setNome(scan.nextLine());
+        novoVendedor.setNome(input.nextLine());
         System.out.print("Digite o RG do vendedor: ");
-        novoVendedor.setRG(scan.nextLong());
+        novoVendedor.setRG(input.nextLong());
         System.out.println("Digite a data de nascimento do vendedor: ");
         int dia, mes, ano;
         do{
             System.out.print("Dia: ");
-            dia = scan.nextInt();
+            dia = input.nextInt();
         }while(!Data.validarDia(dia));
 
         do{
             System.out.print("Mês: ");
-            mes = scan.nextInt();
+            mes = input.nextInt();
         }while(!Data.validarMes(mes));
 
         do{
             System.out.print("Ano: ");
-            ano = scan.nextInt();
+            ano = input.nextInt();
         }while(!Data.validarAno(ano));
 
         Data dataNasc = new Data(dia, mes, ano);
@@ -672,31 +674,240 @@ public class Sistema {
         System.out.println("Digite a data de admissão o vendedor: ");
         do{
             System.out.print("Dia: ");
-            dia = scan.nextInt();
+            dia = input.nextInt();
         }while(!Data.validarDia(dia));
 
         do{
             System.out.print("Mês: ");
-            mes = scan.nextInt();
+            mes = input.nextInt();
         }while(!Data.validarMes(mes));
 
         do{
             System.out.print("Ano: ");
-            ano = scan.nextInt();
+            ano = input.nextInt();
         }while(!Data.validarAno(ano));
 
         Data dataAdmissao = new Data(dia, mes, ano);
         novoVendedor.setDataAdmissao(dataAdmissao);
 
         System.out.print("Digite o salário do vendedor:\nR$");
-        novoVendedor.setSalario(scan.nextFloat());
+        novoVendedor.setSalario(input.nextFloat());
 
         System.out.print("Digite o tempo de experiência restante:");
-        novoVendedor.setTempoRestante(scan.nextFloat());
+        novoVendedor.setTempoRestante(input.nextFloat());
 
-        //Fata Adicionar gerente
+        System.out.print("Digite o login do gerente responsável: ");
+        input.nextLine();
+		String login_adm = input.nextLine();
+		Gerente pesquisa = Sistema.getMapGerentes().get(login_adm);
+        if(pesquisa != null){
+            novoVendedor.setGerente(pesquisa);
+        }
+        else{
+            System.out.print("Digite um login válido!");
+        }
 
-        //listaVendedores.add(novoVendedor);
+        listaVendedores.add(novoVendedor);
+
+        System.out.println("Crie um login e senha para o vendedor:");
+        input.nextLine();
+        System.out.print("Login: ");
+        String login = input.nextLine();
+        do{
+            System.out.print("Senha: ");
+            senha = input.nextLine();
+            System.out.print("Confirme sua senha: ");
+            confirSenha = input.nextLine();
+            if(!senha.equals(confirSenha))
+                System.out.println("As senhas digitadas não são iguais! Tente novamente");
+        }while(!senha.equals(confirSenha));
+        novoVendedor.setLogin(login);
+        novoVendedor.setSenha(senha);
+
+        escreverVendedoresNoArquivo();
+    }
+
+    public void escreverVendedoresNoArquivo(){
+        try{            
+            
+            FileWriter escritor = new FileWriter(arqVendedor, true);
+            for(int i = 0; i < listaVendedores.size(); i++){
+                escritor.write(listaVendedores.get(i).getNome() + "\n");
+                escritor.write(listaVendedores.get(i).getRG() + "\n");
+                escritor.write(listaVendedores.get(i).getDataNasc() + "\n");
+                escritor.write(listaVendedores.get(i).getDataAdmissao() + "\n");
+                escritor.write(listaVendedores.get(i).getSalario() + "\n");
+                escritor.write(listaVendedores.get(i).getTempoRestante() + "\n");
+                escritor.write(listaVendedores.get(i).getGerente().getNome() + "\n");
+                escritor.write("\n");
+            }                    
+            escritor.close();                        
+        }
+        catch(IOException e)
+        {
+            System.out.println("Erro" + e);
+        }
+    }
+
+    public void listarVendedores(){
+        for(int i = 0; i < listaVendedores.size(); i++){
+            System.out.println("Nome: " + listaVendedores.get(i).getNome());
+            System.out.println("RG: " + listaVendedores.get(i).getRG());
+            System.out.println("Data de Nascimento: " + listaVendedores.get(i).getDataNasc());
+            System.out.println("Data de Admissão: " + listaVendedores.get(i).getDataAdmissao());
+            System.out.println("Salário: " + listaVendedores.get(i).getSalario());
+            System.out.println("Tempo de Experiência Restante: " + listaVendedores.get(i).getTempoRestante() + "h");
+            System.out.println("Gerente Responsável: " + listaVendedores.get(i).getGerente().getNome());
+        }
+    }
+
+    public void alterarVendedor(){
+        int op;
+        int dia, mes, ano;
+        Data novaData;
+        String senha,confirSenha;
+        listarVendedores();
+        System.out.println("Qual vendedor deseja alterar?");
+        vendedor = (input.nextInt() - 1);
+        if (listaMotocicletas.size() == 0) 
+            System.out.println("\nNão há motocicletas cadastradas.");
+        else{
+            do{
+                System.out.println("\nQual dado deseja alterar?");
+                System.out.println("1 - Alterar Nome");
+                System.out.println("2 - Alterar RG");
+                System.out.println("3 - Alterar Data de Nascimento");
+                System.out.println("4 - Alterar Data de Admissão");
+                System.out.println("5 - Alterar Salário");
+                System.out.println("6 - Alterar Tempo restante de experiência");
+                System.out.println("7 - Alterar Gerente Responsável");
+                System.out.println("8 - Alterar login");
+                System.out.println("9 - Alterar senha");
+                System.out.println("10 - Sair");
+                System.out.println("\nDigite a opção desejada: ");
+                op = input.nextInt();
+                input.nextLine();
+                
+                switch(op){
+                    case 1:
+                        System.out.print("\nDigite o novo nome: ");
+                        listaVendedores.get(vendedor).setNome(input.nextLine());  
+                        input.nextLine();  
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 2:
+                        System.out.print("\nDigite o novo RG: ");
+                        listaVendedores.get(vendedor).setRG(input.nextLong());  
+                        input.nextLine();  
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 3:
+                        System.out.println("\nDigite a nova data de nascimento: ");
+                        do{
+                            System.out.print("Dia: ");
+                            dia = input.nextInt();
+                        }while(!Data.validarDia(dia));
+
+                        do{
+                            System.out.print("Mês: ");
+                            mes = input.nextInt();
+                        }while(!Data.validarMes(mes));
+
+                        do{
+                            System.out.print("Ano: ");
+                            ano = input.nextInt();
+                        }while(!Data.validarAno(ano));
+                        novaData = new Data(dia, mes, ano);
+                        listaVendedores.get(vendedor).setDataNasc(novaData);
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 4:
+                        System.out.println("\nDigite a nova data de admissão: ");
+                        do{
+                            System.out.print("Dia: ");
+                            dia = input.nextInt();
+                        }while(!Data.validarDia(dia));
+
+                        do{
+                            System.out.print("Mês: ");
+                            mes = input.nextInt();
+                        }while(!Data.validarMes(mes));
+
+                        do{
+                            System.out.print("Ano: ");
+                            ano = input.nextInt();
+                        }while(!Data.validarAno(ano));
+                        novaData = new Data(dia, mes, ano);
+                        listaVendedores.get(vendedor).setDataAdmissao(novaData);
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 5:
+                        System.out.print("\nDigite o novo Salário: R$ ");
+                        listaVendedores.get(vendedor).setSalario(input.nextLong());  
+                        input.nextLine();  
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 6:
+                        System.out.print("\nDigite o novo tempo restante: ");
+                        listaVendedores.get(vendedor).setSalario(input.nextInt());  
+                        input.nextLine();  
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 7:
+                        System.out.print("\nDigite o login do novo gerente responsável: ");
+                        input.nextLine();
+                        String login_adm = input.nextLine();
+                        Gerente pesquisa = Sistema.getMapGerentes().get(login_adm);
+                        if(pesquisa != null){
+                            listaVendedores.get(vendedor).setGerente(pesquisa);
+                            System.out.println("\n\n==> Dados atualizados com sucesso!");
+                        }
+                        else{
+                            System.out.print("Digite um login válido!");
+                        }
+                    break;
+                    case 8:
+                        System.out.print("\nDigite o novo login: ");
+                        listaVendedores.get(vendedor).setLogin(input.nextLine());  
+                        input.nextLine();  
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+                    case 9:
+                        do{
+                            System.out.print("Digite a nova senha: ");
+                            senha = input.nextLine();
+                            System.out.print("Confirme a senha: ");
+                            confirSenha = input.nextLine();
+                            if(!senha.equals(confirSenha))
+                                System.out.println("As senhas digitadas não são iguais! Tente novamente");
+                        }while(!senha.equals(confirSenha));
+                        listaVendedores.get(vendedor).setSenha(senha);
+                        System.out.println("\n\n==> Dados atualizados com sucesso!");
+                    break;
+
+                }
+            }while(op != 10);
+    }
+    arqVendedor.delete();
+    escreverVendedoresNoArquivo();
+    }
+
+    public void removerVendedor(){
+        listarVendedores();
+        System.out.println("Qual vendedor deseja excluir?");
+        vendedor = (input.nextInt() - 1);		
+		input.nextLine();		
+
+		if (listaVendedores.size() == 0) {
+			System.out.println("\nNão há vendedores cadastrados.");            
+		}
+        else{
+            listaVendedores.remove(vendedor);
+		    System.out.println("\n\nVendedor excluído com sucesso!");
+        }
+
+        arqVendedor.delete();
+        escreverVendedoresNoArquivo();
 
     }
 }
